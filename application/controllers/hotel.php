0fff14abbc9c4eaff0b681e9b7c4f60edd689e91
cagri <selam@bencagri.com>
2015-02-13 14:34:13 +0200

    total nights added

--- application/controllers/hotel.php
+++ application/controllers/hotel.php
@@ -15,16 +15,21 @@ class Hotel extends CI_Controller {
 		if (!$hotel_id) {
 			exit('Error');
 		}
+		//get hotel info
+		$hotel = $this->front_model->hotel_info($hotel_id);
 
-		$default_lang = $this->session->userdata('default_lang') ? $this->session->userdata('default_lang') : 'de';
+		if (!$hotel) {
+			exit('Hotel not found.');
+		}
+
+		$default_lang = $this->session->userdata('default_lang') ? $this->session->userdata('default_lang') : 'en';
 
 		$start_date = $this->input->get('checkin') ? $this->input->get('checkin') : date('Y-m-d');
 		$end_date 	= $this->input->get('checkout') ? $this->input->get('checkout') : date('Y-m-d');
 		$adults		= $this->input->get('adults') ? $this->input->get('adults') : '1';
 		$children	= $this->input->get('child') ? $this->input->get('child') : '0';
-
-		//get hotel info
-		$hotel = $this->front_model->hotel_info($hotel_id);
+		$nights 	= 1;
+		
 
 		//get rooms
 		$search = array('child' => $children,
@@ -38,7 +43,7 @@ class Hotel extends CI_Controller {
 		foreach (date_range($start_date,$end_date) as $k => $d) {
 
 			$arr['dates'][$d] = $d;
-		
+			$nights = count($arr['dates']);
 			foreach ($rooms as $key => $r) {
 				$arr['rooms'][$r->id]['name'] 		= $r->name;
 				$arr['rooms'][$r->id]['title'] 		= $r->title;
@@ -72,10 +77,11 @@ class Hotel extends CI_Controller {
 
 
 
-		$data['hotel_info'] = $hotel;
-		$data['rooms'] = $rooms;
+		$data['hotel_info'] 	= $hotel;
+		$data['rooms'] 			= $arr['rooms'];
+		$data['promotion'] 		= $arr['promotions'];
 
-
+		echo $nights;
 		echo '<pre>';
 		print_r($arr);
 
