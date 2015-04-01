b127ba50ab35ea72cfcd40548a57fb8a945bf343
cagri <selam@bencagri.com>
2015-04-01 15:12:24 +0300

    query string fix for currencies

--- application/helpers/general_helper.php
+++ application/helpers/general_helper.php
@@ -7,7 +7,11 @@ function current_full_url()
     $CI =& get_instance();
 
     $url = $CI->config->site_url($CI->uri->uri_string());
-    return $_SERVER['QUERY_STRING'] ? $url.'?'.$_SERVER['QUERY_STRING'] : $url;
+    $string  = $_SERVER['QUERY_STRING'];
+    if ($CI->input->get('cur')) {
+    	 $string = substr($string,0,-8);
+    }
+    return  $string ? $url.'?'. $string : $url;
 }
 
 /*
