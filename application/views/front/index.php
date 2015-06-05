69ce7c1bd19219b963dbf24de6ff5a71f8ae44a9
bencagri <selam@bencagri.com>
2015-06-05 15:10:22 +0300

    set user country auto

--- application/views/front/index.php
+++ application/views/front/index.php
@@ -844,8 +844,9 @@ $this->load->view('front/header');
                         <?php $countries = countries(); ?>
                             <select name="country"  class="w-220 b1s-000 mtb-5">
                             <?php foreach ($countries as $key => $country) {
-                              echo '<option value="'.$country->code.'">'.$country->name.'</option>';
-                            } ?>
+                             $select =  (ip_info("Visitor", "Country Code") == $country->code) ? 'selected="selected"' : '';
+                              echo '<option value="'.$country->code.'" '.$selected.'>'.$country->name.'</option>';
+                            }?>
                             </select>
                         </div>
                     </div>
